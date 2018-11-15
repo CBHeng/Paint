182722ea67c6cbc41cc1bf464f16957265c844cc
CBoHeng <chenboheng3@gmail.com>
2018-11-16 07:27:05 +0800

    整理無用程式碼

--- src/js/Paint/core/CanvasEventHooks.js
+++ src/js/Paint/core/CanvasEventHooks.js
@@ -1,22 +1,6 @@
 export default class CanvasEventHooks {
     constructor() {
         this.isDraw = false
-
-        this.mouse = {
-            last: {
-                x: 0,
-                y: 0
-            },
-            now: {
-                x: 0,
-                y: 0
-            }
-        }
-
-        this.moveSpace = {
-            x: 0,
-            y: 0
-        }
     }
     _init(canvas) {
         canvas.element.addEventListener('mousedown', this.mousedownDoing.bind(this,canvas))
@@ -26,9 +10,6 @@ export default class CanvasEventHooks {
 
     mousedownDoing(canvas, event) {
         if (!canvas.tooler || this.isDraw) return
-
-        this.mouse.now.x = event.offsetX
-        this.mouse.now.y = event.offsetY
 
         //新增 (當前工具)的全新數據
         let newData = canvas.tooler.data()
@@ -44,28 +25,13 @@ export default class CanvasEventHooks {
     mousemoveDoing(canvas, event) {
         if (!canvas.tooler || !this.isDraw) return
 
-        this.mouse.now.x = event.offsetX
-        this.mouse.now.y = event.offsetY
-
-        this.moveSpace.x = this.mouse.now.x - this.mouse.last.x
-        this.moveSpace.y = this.mouse.now.y - this.mouse.last.y
-
         canvas.tooler.move(canvas, event)
-
-        this.mouse.last.x = event.offsetX
-        this.mouse.last.y = event.offsetY
     }
 
     mouseupDoing(canvas, event) {
         if (!canvas.tooler || !this.isDraw) return
 
-        this.mouse.now.x = event.offsetX
-        this.mouse.now.y = event.offsetY
-
         canvas.tooler.stop(canvas, event)
-
-        this.mouse.last.x = event.offsetX
-        this.mouse.last.y = event.offsetY
 
         this.isDraw = false
     }
